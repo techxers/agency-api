993b5448d88ddda16a0a6bba6c267708557323d7
Collins Kemboi <techxers@gmail.com>
2024-08-18 12:01:11 +0200

    changed title

--- app/swagger.js
+++ app/swagger.js
@@ -1,66 +1,63 @@
 const swaggerJsdoc = require('swagger-jsdoc');
 
 const swaggerDefinition = {
-  openapi: '3.0.0',
-  info: {
-    title: 'My API',
-    version: '1.0.0',
-    description: 'Web Api for Agency Management System',
-    //add tags here   
-    tags:  [
-      {
-        "name": "Users",
-        "description": "Everything about your Agency"
-      },
-      {
-        "name": "Auth",
-        "description": "User authentication and authorization"
-      },
-      {
-        "name": "Growers",
-        "description": "All operations related to growers"
-      },
-      {
-        "name": "Roles",
-        "description": "Role management and operations"
-      },
-      {
-        "name": "Auction Sale",
-        "description": "Manage auction sales"
-      },
-      {
-        "name": "Departments",
-        "description": "Manage departments"
-      },
-      {
-        "name": "Gender",
-        "description": "Operations related to gender information"
-      },
-      {
-        "name": "Location",
-        "description": "Country and county management"
-      },
-      {
-        "name": "Grades",
-        "description": "Coffee Grades"
-      },
-      {
-        "name": "Agents Information",
-        "description": "The agents managing API"
-      }
-    ]
-    
-  },
-  servers: [
-    {
-      url: 'http://localhost:3000',
+    openapi: '3.0.0',
+    info: {
+        title: 'API Spec document for Coffee Agency System',
+        version: '1.0.0',
+        description: 'Web Api for Agency Management System',
+        //add tags here   
+        tags: [{
+                "name": "Users",
+                "description": "Everything about your Agency"
+            },
+            {
+                "name": "Auth",
+                "description": "User authentication and authorization"
+            },
+            {
+                "name": "Growers",
+                "description": "All operations related to growers"
+            },
+            {
+                "name": "Roles",
+                "description": "Role management and operations"
+            },
+            {
+                "name": "Auction Sale",
+                "description": "Manage auction sales"
+            },
+            {
+                "name": "Departments",
+                "description": "Manage departments"
+            },
+            {
+                "name": "Gender",
+                "description": "Operations related to gender information"
+            },
+            {
+                "name": "Location",
+                "description": "Country and county management"
+            },
+            {
+                "name": "Grades",
+                "description": "Coffee Grades"
+            },
+            {
+                "name": "Agents Information",
+                "description": "The agents managing API"
+            }
+        ]
+
     },
-  ],
+    servers: [{
+        url: 'http://localhost:3000',
+    }, ],
 };
 
 const options = {
-  swaggerDefinition,
-  apis: ['./app/server.js'], // Ensure this path is correct and references files with Swagger JSDoc comments
+    swaggerDefinition,
+    apis: ['./app/server.js'], // Ensure this path is correct and references files with Swagger JSDoc comments
 };
 
 const swaggerSpec = swaggerJsdoc(options);
