dbd09f22cae98184ff9fadb48c122d8dc240ec56
Collins Kemboi <techxers@gmail.com>
2024-10-25 23:24:41 +0200

    added bulk generation api

--- app/swagger.js
+++ app/swagger.js
@@ -3,7 +3,7 @@ const swaggerJsdoc = require('swagger-jsdoc');
 const swaggerDefinition = {
     openapi: '3.0.0',
     info: {
-        title: ' Agency System API Specification',
+        title: ' Agency System API Spec',
         version: '1.0.0',
         description: 'Swagger API for Agency Management System',
         //add tags here   
